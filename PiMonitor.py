f269250e9756f1d05c4a6e2a0f5fada343ce9b3c
nanomesher <info@nanomesher.com>
2017-04-11 01:31:00 +0800

    Add files via upload

--- PiMonitor.py
+++ PiMonitor.py
@@ -6,32 +6,42 @@ import time
 import psutil
 from subprocess import *
 
+def GetLANIP():
+   cmd = "ip addr show eth0 | grep inet  | grep -v inet6 | awk '{print $2}' | cut -d '/' -f 1"
+   p = Popen(cmd, shell=True, stdout=PIPE)
+   output = p.communicate()[0]
+   return output
+
+def GetWLANIP():
+   cmd = "ip addr show wlan0 | grep inet  | grep -v inet6 | awk '{print $2}' | cut -d '/' -f 1"
+   p = Popen(cmd, shell=True, stdout=PIPE)
+   output = p.communicate()[0]
+   return output
+
+
 def GetTemp():
-    #cmd = "sudo wifi scan"
     cmd = "/opt/vc/bin/vcgencmd measure_temp | sed -s 's/=/:/g'"
     p = Popen(cmd, shell=True, stdout=PIPE)
     output = p.communicate()[0]
-    return output.replace('\n','').replace('temp:','Temp:').replace("'C","c")
+    return output.replace('\n','').replace('temp:','').replace("'C","c")
 
-def GetMem():
-    #cmd = "sudo wifi scan"
-    cmd = "cat /proc/meminfo | grep MemFree | sed -s 's/ //g'"
-    p = Popen(cmd, shell=True, stdout=PIPE)
-    output = p.communicate()[0]
-    return output.replace('\n','')
 
 def GetFreeMem():
-    return "Free:" + str(psutil.virtual_memory().free/1024/1024) + "M"
+    return str(psutil.virtual_memory().free/1024/1024) + "M"
 
 def GetCpuPercent():
-    return "CPU:" + str(psutil.cpu_percent()) + "%"
+    return str(psutil.cpu_percent()) + "%"
 
-def PublishToDisplay(text):
+def GetFreeDisk():
+    return str(psutil.disk_usage('/home/pi').free/1024/1024) + "M"
+
+def PublishToDisplay(text,label_prefix):
+    try:
       data = {}
       data['p'] = '0'
-      data['n'] = sys.argv[2]
+      data['n'] = label_prefix + sys.argv[2]
       data['v'] = text
-      data['fc'] = '12345'
+      data['fc'] = '23555'
       url_values = urllib.urlencode(data)
       #url = "http://" + sys.argv[1] + "/TextSetText?p=0&n=" + sys.argv[2] + "&v=" + text + "&fc=12345"
       url = "http://" + sys.argv[1] + "/TextSetText?" + url_values
@@ -39,13 +49,26 @@ def PublishToDisplay(text):
       # print(url)
       response = urllib2.urlopen(url)
       html = response.read()
-      
+    except:
+      pass
 
 
 while(True):
     tempst = GetTemp()
-    PublishToDisplay(tempst)
-    time.sleep(10)
-    cpupercent = GetCpuPercent() + " " + GetFreeMem()
-    PublishToDisplay(cpupercent)
+    PublishToDisplay(tempst,"temp")
+    cpupercent = GetCpuPercent()
+    PublishToDisplay(cpupercent,"cpu")
+    freemem = GetFreeMem()
+    PublishToDisplay(freemem,"mem")
+    freedisk = GetFreeDisk()
+    PublishToDisplay(freedisk,"disk")
+
+    ip = GetLANIP()
+    
+    if(ip == ""):
+        PublishToDisplay(GetWLANIP(),"ip")
+    else:
+        PublishToDisplay(ip,"ip")
+    
+    
     time.sleep(10)
