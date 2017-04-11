72a486f8d817e299685d8010e8c2b3c74a763466
nanomesher <info@nanomesher.com>
2017-04-12 01:28:19 +0800

    Add threshold check and warnings

--- PiMonitor.py
+++ PiMonitor.py
@@ -27,25 +27,42 @@ def GetTemp():
 
 
 def GetFreeMem():
-    return str(psutil.virtual_memory().free/1024/1024) + "M"
+    return psutil.virtual_memory().free/1024/1024
 
 def GetCpuPercent():
     return str(psutil.cpu_percent()) + "%"
 
 def GetFreeDisk():
-    return str(psutil.disk_usage('/home/pi').free/1024/1024) + "M"
+    return psutil.disk_usage('/home/pi').free/1024/1024
 
-def PublishToDisplay(text,label_prefix):
+def LedOn():
+    try:
+      
+      url = "http://" + sys.argv[1] + "/LedOn"
+      response = urllib2.urlopen(url)
+      html = response.read()
+    except:
+      pass
+
+def LedOff():
+    try:
+      
+      url = "http://" + sys.argv[1] + "/LedOff"
+      response = urllib2.urlopen(url)
+      html = response.read()
+    except:
+      pass
+
+
+def PublishToDisplay(text,label_prefix,color):
     try:
       data = {}
       data['p'] = '0'
       data['n'] = label_prefix + sys.argv[2]
       data['v'] = text
-      data['fc'] = '23555'
+      data['fc'] = color
       url_values = urllib.urlencode(data)
-
       url = "http://" + sys.argv[1] + "/TextSetText?" + url_values
-
       response = urllib2.urlopen(url)
       html = response.read()
     except:
@@ -54,20 +71,41 @@ def PublishToDisplay(text,label_prefix):
 
 while(True):
     tempst = GetTemp()
-    PublishToDisplay(tempst,"temp")
+    PublishToDisplay(tempst,"temp","23555")
     cpupercent = GetCpuPercent()
-    PublishToDisplay(cpupercent,"cpu")
+    PublishToDisplay(cpupercent,"cpu","23555")
+
     freemem = GetFreeMem()
-    PublishToDisplay(freemem,"mem")
+
+    if(freemem > 100):
+       PublishToDisplay(str(freemem) + "M","mem","23555")
+       LedOff()
+    else:
+       PublishToDisplay(str(freemem) + "M","mem","61455")
+       LedOn()
+       
     freedisk = GetFreeDisk()
-    PublishToDisplay(freedisk,"disk")
+
+    if(freedisk > 500):
+       PublishToDisplay(str(freedisk) + "M","disk","23555")
+       LedOff()
+    else:
+       PublishToDisplay(str(freedisk) + "M","disk","61455")
+       LedOn()
+
+    if(freemem > 100 and freedisk > 500):
+       LedOff()
+    else:
+       LedOn()
+
+   
 
     ip = GetLANIP()
     
     if(ip == ""):
-        PublishToDisplay(GetWLANIP(),"ip")
+        PublishToDisplay(GetWLANIP(),"ip","23555")
     else:
-        PublishToDisplay(ip,"ip")
+        PublishToDisplay(ip,"ip","23555")
     
     
     time.sleep(10)
